5f8660776be2a79e8c9f551750e6220363d6f84d
RulerPrime <irletrondan@gmail.com>
2025-03-23 18:50:14 -0400

    update

--- script.js
+++ script.js
@@ -1,64 +1,59 @@
 import musicTools from "./MusicTools.js";
 
-const myHTMLpage = document;
+// ------------------------- Get HTML Elements --------------------------
+let midiInput = document.getElementById("midiInput");
+let midiToFreqButton = document.getElementById("midiToFreq");
+let freqResult = document.getElementById("freqResult");
 
-document.getElementById("midiToFreq").addEventListener("click", () => {
-  const midiPitch = parseFloat(document.getElementById("midiInput").value);
-  const resultElement = document.getElementById("freqResult");
+let freqInput = document.getElementById("freqInput");
+let freqToMidiButton = document.getElementById("freqToMidi");
+let midiResult = document.getElementById("midiResult");
+
+let dbfsInput = document.getElementById("dbfsInput");
+let dbfsToLinearButton = document.getElementById("dbfsToLinear");
+let linearResult = document.getElementById("linearResult");
+
+let linearInput = document.getElementById("linearInput");
+let linearToDbfsButton = document.getElementById("linearToDbfs");
+let dbfsResult = document.getElementById("dbfsResult");
+
+// ------------------------- Add Event Listeners --------------------------
+midiToFreqButton.addEventListener("click", () => {
+  let midiPitch = parseFloat(midiInput.value);
   if (!isNaN(midiPitch)) {
-    resultElement.textContent =
+    freqResult.innerText =
       musicTools.midiPitchToFrequency(midiPitch).toFixed(2) + " Hz";
   } else {
-    resultElement.textContent = "Invalid Input";
+    freqResult.innerText = "Invalid Input";
   }
 });
-const mySpecialbutton = document.getElementById("theButton");
-//access the HTML page eith the document object, uses the .getElementById()
-//method to return the html element that has the id "result"
-//assigns it to a new const call mySpecialButton
-//grabs the button thing we made and stores in the const so we can do stuff
-const resultText = document.getElementById("result");
-/**
-helloFunction updates the inner text scale inside of resultText to be a new random number 
-*/
-const helloFunction = function () {
-  resultText.innerText = `${Math.random()}`;
-};
-//use the .addEventListener()to call the helloFunction everytime we "click" the mySpecialButton.
 
-mySpecialbutton.addEventListener("click", helloFunction);
+freqToMidiButton.addEventListener("click", () => {
+  let frequency = parseFloat(freqInput.value);
+  if (!isNaN(frequency) && frequency > 0) {
+    midiResult.innerText = musicTools
+      .frequencyToMidiPitch(frequency)
+      .toFixed(2);
+  } else {
+    midiResult.innerText = "Invalid Input";
+  }
+});
 
-// document.getElementById("freqToMidi").addEventListener("click", () => {
-//   const frequency = parseFloat(document.getElementById("freqInput").value);
-//   const resultElement = document.getElementById("midiResult");
-//   if (!isNaN(frequency) && frequency > 0) {
-//     resultElement.textContent = musicTools
-//       .frequencyToMidiPitch(frequency)
-//       .toFixed(2);
-//   } else {
-//     resultElement.textContent = "Invalid Input";
-//   }
-// });
+dbfsToLinearButton.addEventListener("click", () => {
+  let dbfs = parseFloat(dbfsInput.value);
+  if (!isNaN(dbfs)) {
+    linearResult.innerText = musicTools.dbfsToLinearAmplitude(dbfs).toFixed(4);
+  } else {
+    linearResult.innerText = "Invalid Input";
+  }
+});
 
-// document.getElementById("dbfsToLinear").addEventListener("click", () => {
-//   const dbfs = parseFloat(document.getElementById("dbfsInput").value);
-//   const resultElement = document.getElementById("linearResult");
-//   if (!isNaN(dbfs)) {
-//     resultElement.textContent = musicTools
-//       .dbfsToLinearAmplitude(dbfs)
-//       .toFixed(4);
-//   } else {
-//     resultElement.textContent = "Invalid Input";
-//   }
-// });
-
-// document.getElementById("linearToDbfs").addEventListener("click", () => {
-//   const linear = parseFloat(document.getElementById("linearInput").value);
-//   const resultElement = document.getElementById("dbfsResult");
-//   if (!isNaN(linear) && linear > 0) {
-//     resultElement.textContent =
-//       musicTools.linearAmplitudeTodBFS(linear).toFixed(2) + " dBFS";
-//   } else {
-//     resultElement.textContent = "Invalid Input";
-//   }
-// });
+linearToDbfsButton.addEventListener("click", () => {
+  let linear = parseFloat(linearInput.value);
+  if (!isNaN(linear) && linear > 0) {
+    dbfsResult.innerText =
+      musicTools.linearAmplitudeTodBFS(linear).toFixed(2) + " dBFS";
+  } else {
+    dbfsResult.innerText = "Invalid Input";
+  }
+});
